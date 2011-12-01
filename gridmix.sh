a247627987ed46d3846273bf952bac90ed3e1e60
Thomas A. de Ruiter <thomas@de-ruiter.cx>
2011-12-01 15:36:28 +0100

    indentation

--- gridmix.sh
+++ gridmix.sh
@@ -6,10 +6,10 @@ SGE_O_WORKDIR="$(pwd)"
 . $SGE_O_WORKDIR/hadoop.sh
 
 GZIP_INPUT_TRACE_FILE="$HADOOP_HOME/mapred/src/contrib/mumak/src/test/data/19-jobs.trace.json.gz"
-INPUT_TRACE="$SGE_O_WORKDIR/unpacked-trace.json"
+INPUT_TRACE="$SGE_O_WORKDIR/trace.json"
 DFS_INPUT_TRACE="trace.json"
 
-IOPATH="iopath"
+IOPATH="new-iopath"
 IOPATH_SIZE="1g"
 
 GRIDMIX_JAR="$HADOOP_HOME/mapred/contrib/gridmix/hadoop-0.21.0-gridmix.jar"
@@ -22,6 +22,6 @@ fi
 $SGE_O_WORKDIR/client.sh dfs -rm  $DFS_INPUT_TRACE
 $SGE_O_WORKDIR/client.sh dfs -rmr $IOPATH
 
-$SGE_O_WORKDIR/client.sh dfs -copyFromLocal $INPUT_TRACE $DFS_INPUT_TRACE
+$SGE_O_WORKDIR/client.sh dfs -copyFromLocal $INPUT_TRACE                                  $DFS_INPUT_TRACE
 $SGE_O_WORKDIR/client.sh jar "$GRIDMIX_JAR" $GRIDMIX_CLASS -generate $IOPATH_SIZE $IOPATH $DFS_INPUT_TRACE
-$SGE_O_WORKDIR/client.sh jar "$GRIDMIX_JAR" $GRIDMIX_CLASS $IOPATH $DFS_INPUT_TRACE
+$SGE_O_WORKDIR/client.sh jar "$GRIDMIX_JAR" $GRIDMIX_CLASS                        $IOPATH $DFS_INPUT_TRACE
