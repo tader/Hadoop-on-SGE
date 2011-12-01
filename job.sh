2a4cbf77b36dc09451a43b270687fa873ba839fc
Thomas A. de Ruiter <thomas@de-ruiter.cx>
2011-12-01 13:34:21 +0100

    be nice

--- job.sh
+++ job.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 #$ -cwd
-#$ -N testing
+#$ -N Hadoop
 #$ -t 1-3
+
+set -e
 
 . $SGE_O_WORKDIR/settings.sh
 . $SGE_O_WORKDIR/hadoop.sh
 
 hadoop_start
 
-sleep 600
+trap "hadoop_stop" INT TERM EXIT
+
+    while [ ! -e "$BASE_PATH/shutdown_please" ]; do
+      sleep 10
+    done
+
+trap - INT TERM EXIT
 
 hadoop_stop
